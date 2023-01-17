b32c796db52431fcc9f998ecc01462087fb1ba26
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-01-17 08:58:20 +0300

    Update FeedbackOptions.styled.js

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -14,7 +14,7 @@ export const FeedbackButton = styled.button`
   border: none;
   cursor: pointer;
   background-color: lightgray;
-  transition: background-color 0.2s ease, color 0.2s ease;
+  transition: background-color 0.3s ease, color 0.3s ease;
 
   :hover,
   :focus {
